4e9e2769ce8653290b11b9a642880699cc07728e
Manin Iliy <bkmzvf@gmail.com>
2012-11-15 12:07:53 +0700

    Public side

--- config/navigations/manager_navigation.rb
+++ config/navigations/manager_navigation.rb
@@ -4,7 +4,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     navigation.selected_class = 'current'
-    primary.item :claims, 'Заявки', manage_expert_claims_path, highlights_on: /^\/manage\/expert_claims/
+    primary.item :claims, 'Заявки участников', manage_expert_claims_path, highlights_on: /^\/manage\/expert_claims/
   end
 
 end
