aeb45c928093836fc2b254d7ca1fc77d2587be46
Manin Iliy <bkmzvf@gmail.com>
2013-11-01 12:14:09 +0700

    fix

--- app/controllers/manage/claims_controller.rb
+++ app/controllers/manage/claims_controller.rb
@@ -4,4 +4,7 @@ class Manage::ClaimsController < Manage::ApplicationController
   actions :index, :update, :destroy
 
   has_scope :with_pending_state, :default => 1
+  has_scope :only_expert_claims, :default => 1 do |controller, scope|
+    scope.where(:type => 'ExpertClaim')
+  end
 end
