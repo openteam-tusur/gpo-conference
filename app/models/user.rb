1098244d37603d45d77ecfd7647cbb23ed9e4e39
PeoneEr <peoneerko@yandex.ru>
2015-11-26 15:29:20 +0600

    show only current projects in dashboard for participant

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,12 @@ class User
   def rated_discourses(conference)
     @rated_discourses ||= rated_projects(conference).flat_map(&:discourses)
   end
+
+  def manager_projects(conference)
+    @manager_projects ||= conference.project_member_claims.where(:user_id => id, :role => :manager).flat_map(&:project).uniq
+  end
+
+  def participant_projects(conference)
+    @participant_projects ||= conference.project_member_claims.where(:user_id => id, :role => :participant).flat_map(&:project).uniq
+  end
 end
